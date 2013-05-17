fixed spline interpolation (now derivative free)

lazyflow/operators/opInterpMissingData.py
@@ -182,7 +182,7 @@ class OpInterpMissingData(Operator):
             F1 = volume[minind+1,...]
             F2 = volume[maxind-1,...]
             F3 = volume[maxind,...]
-            (A0,A1,A2,A3) = _cubic_coeffs_mat(F0,F1,F2,F3)
+            (A0,A1,A2,A3) = _cubic_coeffs_mat(F0,F1,F2,F3,n)
             
             xs = np.linspace(0,1,n+2)
             for i in range(n):
@@ -248,18 +248,27 @@ class OpInterpMissingData(Operator):
 def _spline(a0, a1, a2, a3, x, x2, x3):
     return a0+a1*x+a2*x2+a3*x3
 
-def _cubic_coeffs(f,g,h,i):
+def _cubic_coeffs(f,g,h,i,n=1):
     '''
     cubic interpolation coefficients of vector (x is missing, o is arbitrary)
     oooooofgxxxhioooo
+            |||
+             n times
     '''
-    # CubicInterpolationMatrix = np.matrix("0, 1, 0, 0;-1, 1, 0, 0; 2, -5, 4, -1;-1, 3, -3, 1")
-    #a = g
-    #b = -f+g
-    #c = 2*f-5*g+4*h-i
-    #d = -f+3*g-3*h+i
     
-    return (g,g-f,2*f-5*g+4*h-i,-f+3*g-3*h+i)
+    n = float(n)
+    '''
+    dg = (g-f)/(n+1)
+    dh = (i-h)/(n+1)
+    
+    return (g,dg,-3*g-2*dg+3*h-dh,2*g+dg-2*h+dh)
+    '''
+    # more natural approach:
+    x = [-1/(n+1), 0, 1, (n+2)/(n+1)]
+    A = np.fliplr(np.vander(x))
+    y = np.linalg.solve(A,[f,g,h,i])
+    return (y[0],y[1],y[2],y[3])
+    
     
 _spline_mat = np.vectorize(_spline, otypes=[np.float])
 _cubic_coeffs_mat = np.vectorize(_cubic_coeffs, otypes=[np.float,np.float,np.float,np.float])

tests/testOpInterpMissingData.py
@@ -7,14 +7,15 @@ from lazyflow.operators.opInterpMissingData import OpInterpMissingData
 import unittest
 from numpy.testing import assert_array_almost_equal, assert_array_equal
 
+from scipy.interpolate import UnivariateSpline
 
-np.set_printoptions(precision=3, linewidth=200)
+
+np.set_printoptions(precision=3, linewidth=80)
 
 _testDescriptions = ['large block empty', 'single layer empty', 'last layer empty', 'first block empty', \
                     #'second to last layer empty', 'second layer empty', 'first layer empty', \
                     #'multiple blocks empty', 'all layers empty', 'different regions empty', \
                     ]
-
 
 
 def _getTestVolume(description, method):
@@ -91,14 +92,32 @@ class TestInterpolation(unittest.TestCase):
         pass
     
     def testCubicAlgorithm(self):
-        (v,m,e) = _singleMissingLayer(layer=15, nx=1,ny=1,nz=20,method='cubic')
+        (v,m,orig) = _singleMissingLayer(layer=15, nx=1,ny=1,nz=50,method='cubic')
+        v[:,:,10:15] = 0
+        m[:,:,10:15] = 1
         interpolationMethod = 'cubic'
         g=Graph()
         op = OpInterpMissingData(graph = g)
         op.InputSearchDepth.setValue(0)
         op.interpolationMethod = interpolationMethod
         op.InputVolume.setValue( v )
-        assert_array_almost_equal(op.Output[:].wait()[:].view(np.ndarray), e.view(np.ndarray)[:], decimal=1, err_msg="method='{}', test='{}'".format(interpolationMethod, 'simple cubic interpolation'))
+        
+        # natural comparison
+        assert_array_almost_equal(op.Output[:].wait()[:,:,10:15].view(np.ndarray),\
+                                orig[:,:,10:15].view(np.ndarray), decimal=3,\
+                                err_msg="direct comparison to cubic data")
+        
+        # scipy spline interpolation
+        x = np.zeros(v.shape)
+        x[:,:,:] = np.arange(v.shape[2])
+        (i,j,k) = np.where(m==0)
+        xs = x[i,j,k]
+        ys = v.view(np.ndarray)[i,j,k]
+        spline = UnivariateSpline(x[:,:,[8, 9, 16, 17]], v[:,:,[8,9,16,17]], k=3, s=0)
+        e = spline(np.arange(v.shape[2]))
+        
+        assert_array_almost_equal(op.Output[:].wait()[:,:,10:15].squeeze().view(np.ndarray),\
+                                e[10:15], decimal=3, err_msg="scipy.interpolate.UnivariateSpline comparison")
     
 
 